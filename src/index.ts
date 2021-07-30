99d919425a8d9d6c213f41b3b962eaf197255582
Jackson Wold <jacksonrwold@gmail.com>
2021-07-29 20:34:15 -0500

    Added super basic posting functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world")
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: "lireddit",
+        type: "postgresql",
+        user: "postgres",
+        password: "postgres",
+        debug: !__prod__
+    })
+
+    const post = orm.em.create(Post, {title: "My first post"})
+    await orm.em.persistAndFlush(post)
+}
+
+main()
