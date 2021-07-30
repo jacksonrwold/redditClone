db5832a7a2551d0d1918a979fefbc0fef55b7190
Jackson Wold <jacksonrwold@gmail.com>
2021-07-29 21:01:39 -0500

    regenerated the dist folder

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const Post_1 = require("./entities/Post");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = async () => {
-    console.log(__dirname);
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    await orm.getMigrator().up();
+    const post = orm.em.create(Post_1.Post, { title: "My first post" });
+    await orm.em.persistAndFlush(post);
 };
 main();
 //# sourceMappingURL=index.js.map
